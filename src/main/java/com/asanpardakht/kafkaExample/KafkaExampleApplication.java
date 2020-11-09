3cffd4636c6dad0334e1e2cbb26912e283ce6b09
fatemeh <fatemeh.karbalaie@gmail.com>
2020-11-09 12:29:28 +0330

    add simple sample for produce and consume messages via kafka

---
+++ src/main/java/com/asanpardakht/kafkaExample/KafkaExampleApplication.java
@@ -0,0 +1,25 @@
+package com.asanpardakht.kafkaExample;
+
+import com.asanpardakht.kafkaExample.service.SendMessageService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaExampleApplication implements CommandLineRunner {
+
+	private final SendMessageService sendMessageService;
+
+	public KafkaExampleApplication(SendMessageService sendMessageService) {
+		this.sendMessageService = sendMessageService;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaExampleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		sendMessageService.sendMessage("Hello Kafka");
+	}
+}
---
+++ src/main/java/com/asanpardakht/kafkaExample/service/SendMessageService.java
@@ -0,0 +1,43 @@
+package com.asanpardakht.kafkaExample.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@Service
+public class SendMessageService {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value(value = "${message.topic.name}")
+    String topicName;
+
+    public void sendMessage(String message) {
+        ListenableFuture<SendResult<String, String>> future =
+                kafkaTemplate.send(topicName, message);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                System.out.println("Sent message=[" + message +
+                        "] with offset=[" + result.getRecordMetadata().offset() + "]");
+            }
+            @Override
+            public void onFailure(Throwable ex) {
+                System.out.println("Unable to send message=["
+                        + message + "] due to : " + ex.getMessage());
+            }
+        });
+    }
+
+    @KafkaListener(topics = "${message.topic.name}", groupId = "foo")
+    public void listenGroupFoo(String message) {
+        System.out.println("Received Message in group foo: " + message);
+    }
+}
